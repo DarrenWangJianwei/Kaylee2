f080fc0487f797aa2976f142958b9e85e5c357a8
wang Jianwei <darren.wjy1990@gmail.com>
2022-01-22 01:08:55 -0800

    remove useLocation from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import {Routes, Route, useLocation} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/home';
 import FooterComponent from './components/FooterComponent';
